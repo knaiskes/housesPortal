9f83f0c4ba0c5d2bf2206335e2de3db6fc329d96
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-08-28 13:18:20 +0300

    add some basic model's fields

--- rentHouses/houses/models.py
+++ rentHouses/houses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class House(models.Model):
+    owner = models.CharField("Owner Name", max_length=100)
+    email = models.EmailField()
+    phone = models.CharField("Phone number", max_length=30)
+    address = models.CharField("Address", max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.owner
