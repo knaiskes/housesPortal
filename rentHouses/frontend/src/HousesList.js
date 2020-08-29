44f15624a3fdce7ea5cc127b3679324b26c33a0e
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-08-29 17:26:00 +0300

    fix margin between the cards

--- rentHouses/frontend/src/HousesList.js
+++ rentHouses/frontend/src/HousesList.js
@@ -32,27 +32,14 @@ nextPage(){
 render() {
 
     return (
-	    <div classNameName="housesList">
+	    <div classNameName="housesList" style={{display: 'flex'}}>
 	    {this.state.houses.map( c =>
-	     <div class="col-sm-4">
-
-        <div class="card">
-		    <img class="card-img-top img-fluid" src={require('./test.png')}  alt="Card image cap" />
-          <div class="card-block">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-          </ul>
-          <div class="card-block">
-            <a href="#" class="card-link">Card link</a>
-          </div>
-        </div>
-
-      </div>
+		    <div class="card" style={{width: '18rem', margin: '2rem'}}>
+		    <img class="card-img-top" src={require('./test.png')} alt="Card image cap" />
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
 
 	    )}
 	    </div>
