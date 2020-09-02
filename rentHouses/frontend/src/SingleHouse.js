aa13fa69c317ebf675004763bddfeee8c0d5a590
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-09-02 21:30:57 +0300

    Remove carousel inline indicators

--- rentHouses/frontend/src/SingleHouse.js
+++ rentHouses/frontend/src/SingleHouse.js
@@ -50,11 +50,6 @@ class  HousesList  extends  Component {
 
 
 		<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-		<ol className="carousel-indicators">
-		<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-		</ol>
 		<div className="container">
 		<div className="carousel-inner">
 		<div className="carousel-item active">
