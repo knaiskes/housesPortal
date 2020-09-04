7697214ea090de3140d8f537dd2a219cf2d3a731
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-09-04 19:56:51 +0300

    Rename houses app to backend

--- rentHouses/rentHouses/urls.py
+++ rentHouses/rentHouses/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('houses.urls', namespace='houses')),
+    path('api/', include('backend.urls', namespace='backend')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
